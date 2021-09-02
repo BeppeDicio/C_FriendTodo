0bb25c455859a524dc574e06e0c1da53329236ce
Giuseppe Diciolla <giuseppe.diciolla@gmail.com>
2021-09-02 11:04:02 +0200

    [master] add new task view

--- FriendToDo/FriendToDo/View/AddTodoView.swift
+++ FriendToDo/FriendToDo/View/AddTodoView.swift
@@ -10,8 +10,35 @@ import UIKit
 
 class AddTodoView: UIViewController {
     
+    @IBOutlet weak var addTaskButton: UIButton!
+    @IBOutlet weak var taskTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        //TODO: Download function of the friend tasks, every time that this page is reloaded
+        //so that you have every time the list uptodate. At the moment the data reciver has a
+        //view controller dependency and need Refactoring
+        
+        addTaskButton.layer.cornerRadius = 20
+    }
+    
+    func addTaskToList(title: String) {
+        
+        //TODO: create function that creates ToDo obdject and sent the Post to API server
+        //TODO: if the response is 200OK the interface will cleanup and you can inser your next task
+        
+        //Only for user feedback propose
+        taskTextField.text = ""
+        
+    }
+    
+    @IBAction func addTaskButtonAction(_ sender: Any) {
+        
+        let title = taskTextField.text ?? ""
+        
+        if (title != ""){
+            addTaskToList(title: title)
+        }
     }
 }
